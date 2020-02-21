664fd564455d4f06ab8ac557e02c2e1abb48a970
Biking0 <1342831695@qq.com>
2020-02-22 00:32:50 +0800

    demo

---
+++ schedule_run/test.py
@@ -0,0 +1,12 @@
+# encoding=utf8
+# Scheduled to run on Friday
+# by hyn
+# 20200220
+
+import time
+import datetime
+
+print datetime.datetime.now()
+
+
+#/home/ocetl/hyn/test
---
+++ schedule_run/test.sh
@@ -0,0 +1,7 @@
+echo 1
+sleep 3
+echo 2
+
+echo 3
+sleep 3
+echo 4
